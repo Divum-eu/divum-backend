deb91341b34443b136b249ae6c645c193acdcdea
Dimpex0 <mitkodd2005@gmail.com>
2026-03-30 21:31:10 +0300

    created empty spring boot project

---
+++ src/test/java/eu/divum/divumbackend/DivumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.divum.divumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
