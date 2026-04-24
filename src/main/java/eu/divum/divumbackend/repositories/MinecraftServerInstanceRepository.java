174f0c24c1f556048e753317326c0c953e3f1aef
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-04-24 19:53:19 +0300

    feat: added service and repository interfaces for Minecraft server instance creation

---
+++ src/main/java/eu/divum/divumbackend/repositories/MinecraftServerInstanceRepository.java
@@ -0,0 +1,9 @@
+package eu.divum.divumbackend.repositories;
+
+import eu.divum.divumbackend.repositories.interfaces.ServerInstanceRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MinecraftServerInstanceRepository implements ServerInstanceRepository {
+
+}
---
+++ src/main/java/eu/divum/divumbackend/repositories/interfaces/ServerInstanceRepository.java
@@ -0,0 +1,6 @@
+package eu.divum.divumbackend.repositories.interfaces;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface ServerInstanceRepository {
+}
