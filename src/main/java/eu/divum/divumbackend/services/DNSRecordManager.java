4683556583238bc2ee1bdb2bcf51f79d4f2e486a
Dimitar Dimitrov <mitkodd2005@gmail.com>
2026-04-25 18:42:12 +0300

    fix: remove unused imports

--- src/main/java/eu/divum/divumbackend/services/DNSRecordManager.java
+++ src/main/java/eu/divum/divumbackend/services/DNSRecordManager.java
@@ -1,7 +1,5 @@
 package eu.divum.divumbackend.services;
 
-import java.io.IOException;
-
 public interface DNSRecordManager {
     String create(String domain, String ipAddress);
     boolean delete(String domain);
