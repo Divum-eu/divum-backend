a99ff2d37c0cc7dd3c9ffd03ad51b064412230ea
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-04-27 11:36:53 +0300

    feat: added get available servers repository method

--- src/main/java/eu/divum/divumbackend/repositories/ServerMachineRepository.java
+++ src/main/java/eu/divum/divumbackend/repositories/ServerMachineRepository.java
@@ -3,8 +3,15 @@ package eu.divum.divumbackend.repositories;
 import eu.divum.divumbackend.domain.ServerMachine;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ServerMachineRepository extends JpaRepository<ServerMachine, UUID> {
+    @Query("SELECT s FROM ServerMachines s " +
+            "WHERE s.freeCpuCores > :reqCores" +
+            "  AND s.freeRam > :reqRam")
+    List<ServerMachine> findAllAvailable(@Param("reqCores") int cores, @Param("reqRam") int ram);
 }
