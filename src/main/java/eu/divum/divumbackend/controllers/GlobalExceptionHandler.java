e9680a4c49222e09b128e168e263fdb58b2e6050
Dimitar Dimitrov <mitkodd2005@gmail.com>
2026-05-18 20:55:25 +0300

    fix: switch all exception handlers to ProblemDetail

--- src/main/java/eu/divum/divumbackend/controllers/GlobalExceptionHandler.java
+++ src/main/java/eu/divum/divumbackend/controllers/GlobalExceptionHandler.java
@@ -1,14 +1,22 @@
 package eu.divum.divumbackend.controllers;
 
 import eu.divum.divumbackend.dtos.ErrorDto;
+import eu.divum.divumbackend.exceptions.HTTPRequestException;
+import eu.divum.divumbackend.exceptions.minecraftserverinstance.*;
+import eu.divum.divumbackend.exceptions.servermachine.NoAvailableServerMachines;
+import eu.divum.divumbackend.exceptions.servermachine.NotEnoughServerResources;
 import eu.divum.divumbackend.exceptions.user.UserNotFound;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+import java.net.URI;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(UserNotFound.class)
     public ResponseEntity<ErrorDto> handleUserNotFoundException() {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto("User not found."));
@@ -17,5 +25,103 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorDto> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(new ErrorDto(e.getMessage().isBlank() ? "Invalid request body." : e.getMessage()));
+    }
+
+    @ExceptionHandler(HTTPRequestException.class)
+    public ProblemDetail handleHTTPRequestException() {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                "A call to an external API failed."
+        );
+    }
+
+    @ExceptionHandler(MinecraftServerInstanceNotFound.class)
+    public ProblemDetail handleMinecraftServerInstanceNotFound(MinecraftServerInstanceNotFound ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Minecraft server instance not found.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MinecraftServerInstanceStartFailed.class)
+    public ProblemDetail handleMinecraftInstanceStartFailed(MinecraftServerInstanceStartFailed ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Couldn't start Minecraft instance.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MinecraftServerInstanceStopFailed.class)
+    public ProblemDetail handleMinecraftServerInstanceStopFailed(MinecraftServerInstanceStopFailed ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Couldn't stop Minecraft instance.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MinecraftServerInstanceDeleteFailed.class)
+    public ProblemDetail handleMinecraftServerInstanceDeleteFailed(MinecraftServerInstanceDeleteFailed ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Couldn't delete Minecraft instance.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MinecraftServerInstanceWithSameAddressExists.class)
+    public ProblemDetail handleMinecraftServerInstanceWithSameAddressExists(MinecraftServerInstanceWithSameAddressExists ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Minecraft server instance with the same address exists.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MinecraftServerInstanceCreationFailed.class)
+    public ProblemDetail handleMinecraftServerInstanceCreationFailed(MinecraftServerInstanceCreationFailed ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Couldn't create Minecraft instance.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MinecraftServerInstanceUpdateFailed.class)
+    public ProblemDetail handleMinecraftServerInstanceUpdateFailed(MinecraftServerInstanceUpdateFailed ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Couldn't update Minecraft instance.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(NoAvailableServerMachines.class)
+    public ProblemDetail handleNoAvailableServerMachines(NoAvailableServerMachines ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("No server machines for the given RAM and CPU requirements are available.");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(NotEnoughServerResources.class)
+    public ProblemDetail handleNotEnoughServerResources(NotEnoughServerResources ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Not enough server resources for the given RAM and CPU requirements are available.");
+        return problemDetail;
     }
 }
