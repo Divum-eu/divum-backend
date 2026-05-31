c429973e2e7b3b883337949b6d5fa8d0b6538748
Dimitar Dimitrov <mitkodd2005@gmail.com>
2026-05-31 17:33:23 +0300

    feat: add class for managing websocket connections

---
+++ src/main/java/eu/divum/divumbackend/services/implementations/MinecraftInstanceStatusSubscriptionManagerImpl.java
@@ -0,0 +1,56 @@
+package eu.divum.divumbackend.services.implementations;
+
+import eu.divum.divumbackend.services.MinecraftInstanceStatusSubscriptionManager;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Service
+public class MinecraftInstanceStatusSubscriptionManagerImpl implements MinecraftInstanceStatusSubscriptionManager {
+
+    private final ConcurrentHashMap<String, Set<WebSocketSession>> subscriptions = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, String> instanceDaemonMap = new ConcurrentHashMap<>();
+
+    @Override
+    public void addSubscriber(String instanceId, String daemonUrl, WebSocketSession session) {
+        instanceDaemonMap.put(instanceId, daemonUrl);
+        subscriptions.computeIfAbsent(instanceId, _ -> new CopyOnWriteArraySet<>()).add(session);
+    }
+
+    @Override
+    public void removeSubscriber(String instanceId, WebSocketSession session) {
+        Set<WebSocketSession> sessions = subscriptions.get(instanceId);
+        if (sessions != null && session != null) {
+            sessions.remove(session);
+            if (sessions.isEmpty()) {
+                subscriptions.remove(instanceId);
+                instanceDaemonMap.remove(instanceId);
+            }
+        }
+    }
+
+    @Override
+    public boolean hasSubscribersForDaemon(String daemonUrl) {
+        return instanceDaemonMap.containsValue(daemonUrl);
+    }
+
+    @Override
+    public void broadcastToSubscribers(String instanceId, String payload) {
+        Set<WebSocketSession> sessions = subscriptions.get(instanceId);
+        if (sessions != null) {
+            TextMessage message = new TextMessage(payload);
+            for (WebSocketSession session : sessions) {
+                if (session.isOpen()) {
+                    try {
+                        session.sendMessage(message);
+                    } catch (IOException ignored) {}
+                }
+            }
+        }
+    }
+}
