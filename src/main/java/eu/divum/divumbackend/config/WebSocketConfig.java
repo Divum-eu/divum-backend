7746b2d15d066b45b0d9c154d1f472ccae74c4e5
Dimitar Dimitrov <mitkodd2005@gmail.com>
2026-05-31 17:38:36 +0300

    feat: register websocket endpoint

--- src/main/java/eu/divum/divumbackend/config/WebSocketConfig.java
+++ src/main/java/eu/divum/divumbackend/config/WebSocketConfig.java
@@ -1,7 +1,8 @@
 package eu.divum.divumbackend.config;
 
-import eu.divum.divumbackend.controllers.FrontendWebSocketHandler;
+import eu.divum.divumbackend.websockets.FrontendWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -12,11 +13,14 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     private final FrontendWebSocketHandler frontendHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(frontendHandler, "/v1/ws/instances/*")
-                .setAllowedOrigins("*"); // TODO: Configure CORS properly for production
+        registry.addHandler(frontendHandler, "/v1/minecraft-servers/*/status")
+                .setAllowedOrigins(allowedOrigins); // TODO: Configure CORS properly for production
     }
 }
