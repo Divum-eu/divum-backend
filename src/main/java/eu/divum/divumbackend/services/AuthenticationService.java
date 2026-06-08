7a36bbd7edca82a0600510a9e4e522ac5bd85ff0
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-06-08 22:13:12 +0300

    feat: add password encoding and disable auto-security

--- src/main/java/eu/divum/divumbackend/services/AuthenticationService.java
+++ src/main/java/eu/divum/divumbackend/services/AuthenticationService.java
@@ -2,4 +2,6 @@ package eu.divum.divumbackend.services;
 
 public interface AuthenticationService {
     String generatePasswordHash(String password);
+
+    Boolean validatePassword(String rawPassword, String hashedPassword);
 }
