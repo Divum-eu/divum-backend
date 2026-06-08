9dc630c7dca3db7defd6c83627ac06a4bdafd08f
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-06-08 22:13:12 +0300

    feat: add password encoding and disable auto-security

--- src/main/java/eu/divum/divumbackend/services/implementations/AuthenticationServiceImpl.java
+++ src/main/java/eu/divum/divumbackend/services/implementations/AuthenticationServiceImpl.java
@@ -4,14 +4,22 @@ import eu.divum.divumbackend.services.AuthenticationService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public String generatePasswordHash(String password) {
-        return "";
+        return passwordEncoder.encode(password);
+    }
+
+    @Override
+    public Boolean validatePassword(String rawPassword, String hashedPassword) {
+        return passwordEncoder.matches(rawPassword, hashedPassword);
     }
 }
