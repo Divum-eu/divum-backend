3ac4a90e5238e9772a95313f4e0fbba322325791
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-06-08 22:12:23 +0300

    feat: implement User CRUD operations and API endpoints

--- src/main/java/eu/divum/divumbackend/services/UserService.java
+++ src/main/java/eu/divum/divumbackend/services/UserService.java
@@ -3,13 +3,14 @@ package eu.divum.divumbackend.services;
 import eu.divum.divumbackend.dtos.user.GetUserResponse;
 import eu.divum.divumbackend.dtos.user.CreateUserRequest;
 import eu.divum.divumbackend.dtos.user.UpdateUserRequest;
+import eu.divum.divumbackend.dtos.user.UpdateUserResponse;
 
 public interface UserService {
     GetUserResponse get(String id);
 
     String create(CreateUserRequest request);
 
-    void update(String id, UpdateUserRequest request);
+    UpdateUserResponse update(String id, UpdateUserRequest request);
 
     void delete(String id);
 }
