3006934e74047ec9c7b576a2e6d5a83d419b3db6
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-06-08 22:34:39 +0300

    fix: remove empty parentheses from SpringBootApplication annotation

--- src/main/java/eu/divum/divumbackend/DivumBackendApplication.java
+++ src/main/java/eu/divum/divumbackend/DivumBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@SpringBootApplication()
+@SpringBootApplication
 public class DivumBackendApplication {
     static void main(String[] args) {
         SpringApplication.run(DivumBackendApplication.class, args);
