2a6369581a0523348613eabd6a38f5485a37acdd
Dimitar Dimitrov <mitkodd2005@gmail.com>
2026-06-13 00:53:22 +0300

    feat: User controller uses current authenticated user

--- src/main/java/eu/divum/divumbackend/controllers/UserController.java
+++ src/main/java/eu/divum/divumbackend/controllers/UserController.java
@@ -8,10 +8,12 @@ import eu.divum.divumbackend.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -19,30 +21,38 @@ import java.net.URI;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<GetUserResponse> getUser(@PathVariable String id) {
-        return ResponseEntity.ok(userService.get(id));
+    @GetMapping("/me")
+    public ResponseEntity<GetUserResponse> getUser(
+            @AuthenticationPrincipal UUID userId
+    ) {
+        return ResponseEntity.ok(userService.get(userId.toString()));
     }
 
-    @PostMapping
-    public ResponseEntity<String> create(@Valid @RequestBody CreateUserRequest createUserRequest, UriComponentsBuilder uriComponentsBuilder) {
-        String userId = userService.create(createUserRequest);
+    // This will be implemented in the future for admins to use
+//    @PostMapping
+//    public ResponseEntity<String> create(@Valid @RequestBody CreateUserRequest createUserRequest, UriComponentsBuilder uriComponentsBuilder) {
+//        String userId = userService.create(createUserRequest);
+//
+//        URI uri = uriComponentsBuilder.path("/v1/users/{id}").buildAndExpand(userId).toUri();
+//
+//        return ResponseEntity.created(uri).body(userId);
+//    }
 
-        URI uri = uriComponentsBuilder.path("/v1/users/{id}").buildAndExpand(userId).toUri();
-
-        return ResponseEntity.created(uri).body(userId);
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<UpdateUserResponse> update(@Valid @RequestBody UpdateUserRequest updateUserRequest, @PathVariable String id) {
-        UpdateUserResponse userResponse = userService.update(id, updateUserRequest);
+    @PatchMapping("/me")
+    public ResponseEntity<UpdateUserResponse> update(
+            @Valid @RequestBody UpdateUserRequest updateUserRequest,
+            @AuthenticationPrincipal UUID userId
+            ) {
+        UpdateUserResponse userResponse = userService.update(userId.toString(), updateUserRequest);
 
         return ResponseEntity.accepted().body(userResponse);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable String id) {
-        userService.delete(id);
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> delete(
+            @AuthenticationPrincipal UUID userId
+    ) {
+        userService.delete(userId.toString());
 
         return ResponseEntity.noContent().build();
     }
